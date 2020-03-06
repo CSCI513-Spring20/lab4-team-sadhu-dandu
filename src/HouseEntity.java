2e89132d97848428b87bebfcc7edaede53941cad
BalajiNaidu-Sadhu <54213777+BalajiNaidu-Sadhu@users.noreply.github.com>
2020-03-05 19:32:04 -0600

    Update HouseEntity.java

--- src/HouseEntity.java
+++ src/HouseEntity.java
@@ -1,4 +1,6 @@
+// Interface Houseentity
 interface HouseEntity {
+//     methods named listhousespecs and countcontents
     public void listHouseSpecs(int level);
     public int countContents();
 }
