39c38136f3d27a68519fdf40e939fa76e88a3fc2
BalajiNaidu-Sadhu <54213777+BalajiNaidu-Sadhu@users.noreply.github.com>
2020-03-05 19:35:56 -0600

    Update HouseFactory.java

--- src/HouseFactory.java
+++ src/HouseFactory.java
@@ -1,4 +1,6 @@
+// Declaring the HouseFactory class as abstract
 public abstract class HouseFactory {
+//     creating the create method with type HouseEntity
     public abstract HouseEntity create(String item);
 
     public HouseEntity buildHouse(String type)
